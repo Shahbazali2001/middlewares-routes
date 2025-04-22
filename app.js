faf2b9a8ecf4b4529d4e3565c27205848e82df18
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-04-23 03:27:31 +0500

    Project Completed

--- app.js
+++ app.js
@@ -19,10 +19,37 @@ app.use((req,res,next)=>{
     next()
 })
 
-app.use((req, res, next)=>{
-    console.log('Third dummy middleware at app level', req.url, req.method)
-    res.send('Hello from Third middleware')
-    next()
+// app.use((req, res, next)=>{
+//     console.log('Third dummy middleware at app level', req.url, req.method)
+//     res.send('Hello from Third middleware')
+//     next()
+// })
+
+app.get('/', (req, res) => { 
+    res.send('Hello, I am handling middlewares')
+})
+
+
+app.get('/contact-us',(req, res, next)=>{
+    console.log('Contact Us', req.url, req.method)
+    res.send(`
+     <h1>Contact Us</h1>   
+    <form action="/contact-us" method="POST">
+        <input type="text" name="name" placeholder="Name">    
+        <br>
+        <input type="email" name="email" placeholder="Email">
+        <br>
+        <input type="password" name="password" placeholder="Password">
+        <br>
+        <button type="submit">Submit</button>
+    </form>`)
+}) 
+
+app.post('/contact-us',(req, res, next)=>{ 
+    console.log('Contact Us', req.url, req.method)
+    res.send(`
+     <h1>Thank You</h1>
+    `)
 })
 
 
