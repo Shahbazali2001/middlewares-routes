b42624986c8cff6374b6f0e87e4b728a72da7d3a
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-04-23 02:37:33 +0500

    Third Middleware

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log('Server is running on port 3000')
+
+
+// app.get('/',(req,res) => {
+
+//     res.send('Hello World! ')
+// }) 
+
+app.use((req,res,next)=>{
+    console.log('First dummy middleware at app level', req.path, req.method)
+    next()
+})
+
+app.use((req,res,next)=>{
+    console.log('Second dummy middleware at app level', req.url, req.method)
+    next()
+})
+
+app.use((req, res, next)=>{
+    console.log('Third dummy middleware at app level', req.url, req.method)
+    res.send('Hello from Third middleware')
+    next()
 })
 
 
 
-app.get('/',(req,res) => {
-
-    res.send('Hello World! ')
-}) 
+// Start the server
+app.listen(port, () => {
+    console.log('Server is running on port 3000')
+})
